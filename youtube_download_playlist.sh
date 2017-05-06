ae767380373e05d3e226a73c638f8ec62ef8c609
Marcin Nowicki <pr0d1r2@gmail.com>
2017-05-06 08:14:46 +0200

    Ensure youtube-dl command present

--- youtube_download_playlist.sh
+++ youtube_download_playlist.sh
@@ -1,4 +1,5 @@
 function youtube_download_playlist() {
   tor_service ##Darwin
+  ensure_command youtube-dl || return $?
   torsocks youtube-dl --proxy socks5://127.0.0.1:9050 -o '%(playlist)s - %(playlist_id)s/%(playlist_index)s - %(id)s - %(title)s.%(ext)s' $@
 }
