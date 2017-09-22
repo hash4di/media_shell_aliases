43284b9937ad21ad4100b29c37a1f13f4bdc95bd
Marcin Nowicki <pr0d1r2@gmail.com>
2017-09-22 23:28:56 +0200

    Do not use playlist index as it duplicates files over time

--- youtube_download_playlist.sh
+++ youtube_download_playlist.sh
@@ -1,5 +1,5 @@
 function youtube_download_playlist() {
   tor_service ##Darwin
   ensure_command_updated youtube-dl || return $?
-  torsocks youtube-dl --proxy socks5://127.0.0.1:9050 -o '%(playlist)s - %(playlist_id)s/%(playlist_index)s - %(id)s - %(title)s.%(ext)s' $@
+  torsocks youtube-dl --proxy socks5://127.0.0.1:9050 -o '%(playlist)s - %(playlist_id)s/%(id)s - %(title)s.%(ext)s' "$@"
 }
