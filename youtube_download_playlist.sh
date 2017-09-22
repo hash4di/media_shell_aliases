ee98255b46345633f1f22a73a36b475984d8f229
Marcin Nowicki <pr0d1r2@gmail.com>
2017-09-22 23:29:22 +0200

    Return error value

--- youtube_download_playlist.sh
+++ youtube_download_playlist.sh
@@ -2,4 +2,5 @@ function youtube_download_playlist() {
   tor_service ##Darwin
   ensure_command_updated youtube-dl || return $?
   torsocks youtube-dl --proxy socks5://127.0.0.1:9050 -o '%(playlist)s - %(playlist_id)s/%(id)s - %(title)s.%(ext)s' "$@"
+  return $?
 }
