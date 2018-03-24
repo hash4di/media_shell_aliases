071a46771c9cfe6de259ed9312a3e06a9f284111
Marcin Nowicki <pr0d1r2@gmail.com>
2018-03-24 06:30:50 +0100

    Use separate share for YouTube downloads

--- ytd.sh
+++ ytd.sh
@@ -1,6 +1,7 @@
 function ytd() {
-  if [ -d /Volumes/Storage/Downloads ]; then
-    cd /Volumes/Storage/Downloads || return $?
+  youtube_mount || return $?
+  if [ -d /Volumes/YouTube ]; then
+    cd /Volumes/YouTube || return $?
   elif [ -d "$HOME/Downloads" ]; then
     cd "$HOME/Downloads" || return $?
   fi
