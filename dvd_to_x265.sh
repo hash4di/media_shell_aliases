cf017403f61f118d629ea747ffd876061cbb9d73
Marcin Nowicki <pr0d1r2@gmail.com>
2018-03-30 08:26:16 +0200

    Conform to ShellCheck

--- dvd_to_x265.sh
+++ dvd_to_x265.sh
@@ -1,17 +1,14 @@
 function dvd_to_x265() {
   local dvd_to_x265_DIR
-  local dvd_to_x265_FILE
-  for dvd_to_x265_DIR in $@
+  for dvd_to_x265_DIR in "$@"
   do
-    if [ -d $dvd_to_x265_DIR ]; then
-      if [ -d $dvd_to_x265_DIR/VIDEO_TS ]; then
-        local dvd_to_x265_INPUT="concat:"
-        for dvd_to_x265_FILE in `find $dvd_to_x265_DIR/VIDEO_TS -iname "*.vob" -type f`
-        do
-          dvd_to_x265_INPUT="$dvd_to_x265_INPUT|$dvd_to_x265_FILE"
-        done
-        dvd_to_x265_INPUT=`echo $dvd_to_x265_INPUT | sed -e 's/concat:|/concat:/'`
-        echorun ffmpeg -i "$dvd_to_x265_INPUT" -c:v libx265 -preset medium -crf 28 -c:a copy -threads 0 $dvd_to_x265_DIR.x265.mp4 || \
+    if [ -d "$dvd_to_x265_DIR" ]; then
+      if [ -d "$dvd_to_x265_DIR/VIDEO_TS" ]; then
+        local dvd_to_x265_INPUT
+        dvd_to_x265_INPUT="$(find "$dvd_to_x265_DIR/VIDEO_TS" -iname "*.vob" -type f | tr "\n" "|")"
+        dvd_to_x265_INPUT="concat:$dvd_to_x265_INPUT"
+        dvd_to_x265_INPUT="${dvd_to_x265_INPUT/concat:|concat:/}"
+        echorun ffmpeg -i "$dvd_to_x265_INPUT" -c:v libx265 -preset medium -crf 28 -c:a copy -threads 0 "$dvd_to_x265_DIR.x265.mp4" || \
           echo "Note that you may want to reinstall ffmpeg with 'brew reinstall ffmpeg --with-x265'"
       else
         echo
